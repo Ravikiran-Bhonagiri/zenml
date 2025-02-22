b2ab23269b8c0cea8504b1fdb9d003bb4a93fde8
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2025-02-21 21:53:32 -0600

    Update aws_image_builder.py

--- src/zenml/integrations/aws/image_builders/aws_image_builder.py
+++ src/zenml/integrations/aws/image_builders/aws_image_builder.py
@@ -11,7 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""AWS Code Build image builder implementation."""
 
 import time
 from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, cast
