caa5a9ba4b0c749170d6b10619818a556ec81fe9
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2025-02-21 21:54:10 -0600

    Update gcp_image_builder.py

--- src/zenml/integrations/gcp/image_builders/gcp_image_builder.py
+++ src/zenml/integrations/gcp/image_builders/gcp_image_builder.py
@@ -11,7 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Google Cloud Builder image builder implementation."""
 
 from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, cast
 from urllib.parse import urlparse
